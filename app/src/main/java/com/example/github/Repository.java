56ab49bb067b2dd9e9d9f8408fec9b66241f0c29
hp <chhabraashish123@gmail.com>
2021-01-08 15:45:11 +0530

    Repository class updated

---
+++ app/src/main/java/com/example/github/Repository.java
@@ -0,0 +1,13 @@
+package com.example.github;
+
+public class Repository {
+    String reponame;
+    String language;
+    String description;
+
+    public Repository(String reponame, String language, String description) {
+        this.reponame = reponame;
+        this.language = language;
+        this.description = description;
+    }
+}
