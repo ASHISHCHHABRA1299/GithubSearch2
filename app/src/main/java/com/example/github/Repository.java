4eaa9ebf97a03c0ede81628e7869554a5e6f1c51
hp <chhabraashish123@gmail.com>
2021-01-08 16:26:59 +0530

    Adapter class updated and profile activity completed

--- app/src/main/java/com/example/github/Repository.java
+++ app/src/main/java/com/example/github/Repository.java
@@ -1,12 +1,12 @@
 package com.example.github;
 
 public class Repository {
-    String reponame;
+    String name;
     String language;
     String description;
 
     public Repository(String reponame, String language, String description) {
-        this.reponame = reponame;
+        this.name = reponame;
         this.language = language;
         this.description = description;
     }
--- app/src/main/java/com/example/github/profileActivity.java
+++ app/src/main/java/com/example/github/profileActivity.java
@@ -1,6 +1,8 @@
 package com.example.github;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -12,8 +14,11 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -24,6 +29,7 @@ public class profileActivity extends AppCompatActivity {
     TextView follower,following,bio;
     String user;
     String url="https://api.github.com/users/";
+    RecyclerView mRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +38,7 @@ public class profileActivity extends AppCompatActivity {
         follower=(TextView)findViewById(R.id.followers);
         following=(TextView)findViewById(R.id.following);
         bio=(TextView)findViewById(R.id.bio);
+        mRecyclerView=(RecyclerView)findViewById(R.id.recyclerview);
         user=getIntent().getStringExtra("Username");
         url=url+user;
         loaddata(url);
@@ -67,6 +74,26 @@ public class profileActivity extends AppCompatActivity {
     }
     public void loadrepository(String url)
     {
-
+        url=url+"/repos?per_page=100&sort=created";
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        GsonBuilder gsonBuilder=new GsonBuilder();
+                        Gson gson=gsonBuilder.create();
+                        Repository[] repoarray=gson.fromJson(response,Repository[].class);
+                        RepositoryAdapter repositoryAdapter=new RepositoryAdapter(repoarray,profileActivity.this);
+                        mRecyclerView.setLayoutManager(new LinearLayoutManager(profileActivity.this));
+                        mRecyclerView.setAdapter(repositoryAdapter);
+                        mRecyclerView.setHasFixedSize(true);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(profileActivity.this, "Unable to fetch the data", Toast.LENGTH_SHORT).show();
+            }
+        });
+        MySingleton.getInstance(profileActivity.this).addToRequestQueue(stringRequest);
     }
 }
